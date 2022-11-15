aee552f87a3ce11cf2bca9015c3750092404bde0
LunarBullet <lunarbulletstudios@gmail.com>
2022-11-15 18:56:26 -0500

    Intensity done

--- Final Computacion Grafica/Assets/Scripts/Effect2Intensity.cs
+++ Final Computacion Grafica/Assets/Scripts/Effect2Intensity.cs
@@ -6,7 +6,13 @@ using UnityEngine.UI;
 public class Effect2Intensity : MonoBehaviour
 {
     [SerializeField] Material glowMaterial;
-    public float MyIntensity; //between -0.5 and 4 for MAX MIN
+    [SerializeField] Material escudoMaterial;
+    [SerializeField] Material caidaMaterial;
+
+    public float MyIntensityEffect1; //between -0.5 and 4 for MAX MIN
+    public float MyIntensityEffect2; //between -0.5 and 4 for MAX MIN
+
+
     [SerializeField] Slider intensitySlider;
 
 
@@ -19,8 +25,31 @@ public class Effect2Intensity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MyIntensity = intensitySlider.value*-3+2.5f;
+        Efecto1Intensity();
+        Efecto2Intensity();
+        Efecto3Intensity();
 
-        glowMaterial.SetFloat("_Intensity", MyIntensity);
+    }
+
+    void Efecto1Intensity()
+    {
+
+        MyIntensityEffect2 = 0.5f*intensitySlider.value * 8;
+
+        escudoMaterial.SetFloat("_interfecsionfallout", MyIntensityEffect2);
+        escudoMaterial.SetFloat("_Rimfallof", 0.5f*intensitySlider.value*2+0.84f);
+    }
+    void Efecto2Intensity()
+    {
+        MyIntensityEffect1 = intensitySlider.value * -3 + 2.5f;
+
+        glowMaterial.SetFloat("_Intensity", MyIntensityEffect1);
+
+    }
+
+    void Efecto3Intensity()
+    {
+
+        caidaMaterial.SetFloat("_Intensity", intensitySlider.value*3f);
     }
 }
